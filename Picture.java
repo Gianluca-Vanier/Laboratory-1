79b82aafd08d67d123468c1fb5f776e31a2cab91
Gianluca Zambito <gzambito28@gmail.com>
2025-08-26 11:12:47 -0400

    Sun color was changed to blue.

--- Picture.java
+++ Picture.java
@@ -55,7 +55,7 @@ public class Picture
             roof.moveVertical(-60);
             roof.makeVisible();
     
-            sun.changeColor("yellow");
+            sun.changeColor("blue");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
